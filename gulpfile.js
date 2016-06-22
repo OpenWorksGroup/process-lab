3100389c5d2f6749c71efbb9bdc9916f8c1bf2d0
kayaelle <kerri@achievery.com>
2016-06-21 22:33:48 -0400

    Updated file for react

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
-var elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir'),
+    path    = require('path'),
+    gulp    = require('gulp'),
+    webpack = require('webpack'),
+    shell   = require('gulp-shell');
+    require('laravel-elixir-webpack');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,11 +15,48 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
+    
+var PATHS = {
+  app: path.join(__dirname, 'resources/assets/js'),
+  build: path.join(__dirname, 'build')
+};
+
+elixir.config.js.browserify.watchify = {
+    enabled: true,
+    options: {
+        poll: true
+    }
+}
 
 elixir(function(mix) {
-    mix.sass('app.scss');
-    mix.scripts([
-        '../bower/jquery/dist/jquery.js',
-        '../bower/bootstrap-sass/assets/javascripts/bootstrap.min.js'
-    ], 'public/js/vendor.js');
+  mix.phpUnit()
+  .sass('app.scss')
+  .browserSync({
+    proxy: 'processlab.dev'
+  })
+  .scripts([
+    '../bower/jquery/dist/jquery.js',
+    '../bower/bootstrap-sass/assets/javascripts/bootstrap.min.js'
+  ], 'public/js/vendor.js')
+  .webpack('index.jsx', {
+    outputDir: 'public/js',
+    entry: PATHS.app + '/index.jsx',
+    output: {
+      filename: 'components.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['babel?cacheDirectory,presets[]=react,presets[]=es2015'],
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+      })
+    ]
+  });
 });
