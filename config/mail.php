6454a6cb4c3b3acb003c44cd9a115f5ef8fa4f77
kayaelle <kerri@achievery.com>
2016-08-09 15:40:13 -0400

    adding email functionality

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_FROM','email@exampleemail.com'), 'name' => env('MAIL_NAME', 'Sender Name')],
 
     /*
     |--------------------------------------------------------------------------
