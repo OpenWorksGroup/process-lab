ebcaaab7ebcaf82f8eea9554cd55899e8d9c391d
kayaelle <kerri@achievery.com>
2016-08-09 15:39:26 -0400

    Making 404 page static

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,47 @@
-@extends('layouts.adminApp')
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Not Found</title>
 
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-@section('content')
+        <style>
+            html, body {
+                height: 100%;
+            }
 
-<h4>Page Not Found </h4>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
 
-@endsection
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Page Not Found</div>
+            </div>
+        </div>
+    </body>
+</html>
