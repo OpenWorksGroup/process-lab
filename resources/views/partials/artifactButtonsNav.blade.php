901a83f0491d3b8e202eaa86c0d1b26ec3f2df01
kayaelle <kerri@achievery.com>
2016-09-29 18:06:01 -0400

    Adding gh-pages

--- resources/views/partials/artifactButtonsNav.blade.php
+++
@@ -1,8 +0,0 @@
-@if($status == "edit") 
-	<div class="vertical-tb-spacer-20"><a class="btn btn-default" data-toggle="modal" data-target="#publishModal">Publish</a></div>
-@endif
-@if(count($rubric) > 0)
-<div class="vertical-tb-spacer-20">
-	<a class="btn btn-default" href="/review/{{ $contentId }}">Submit for Review</a>
-	</div>
-@endif
--- resources/views/partials/checkFieldsRequired.blade.php
+++
@@ -1,6 +0,0 @@
-<ul>
-	@foreach($fieldsMissing as $field)
-		<li><a href="/artifact/{{ $contentId }}/{{ $field['section_id'] }}">
-		{{ $field['section_title'] }} - {{ $field['field_title'] }}</a></li>
-	@endforeach
-</ul>
