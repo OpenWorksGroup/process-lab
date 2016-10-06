9718d8a51a040c3af6558128c096683d8b15511b
kayaelle <kerri@achievery.com>
2016-10-06 14:21:19 -0700

    Content image not displaying

--- resources/views/partials/artifactContentDisplay.blade.php
+++ resources/views/partials/artifactContentDisplay.blade.php
@@ -33,7 +33,7 @@
                 @endforeach
             @endif
 
-            @if(count($field['files']) > 2)
+            @if(count($field['files']) > 1)
                 <div class="content-files" data-files="{{$field['files']}}"></div>
             @endif
         @endforeach
